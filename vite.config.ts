d73e58e85af90b148a06aa1724a920861d11c67c
Richard Skoda <richard.skoda@atrea.cz>
2023-03-15 18:16:21 +0100

    Add Electron (but cant use yet)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    target: 'chrome91',
+    outDir: 'dist/renderer',
+    emptyOutDir: true,
+    assetsDir: './',
+    rollupOptions: {
+      input: {
+        main: './src/main.ts',
+        renderer: './src/renderer.ts',
+      },
+    },
+  },
 })
