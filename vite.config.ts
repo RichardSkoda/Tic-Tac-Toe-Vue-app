7f1a446d5e44e726e5d17509534bfde52711bbfc
Richard Skoda <richard.skoda@atrea.cz>
2023-03-31 13:13:48 +0200

    Add electron settings.

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    target: 'chrome91',
-    outDir: 'dist/renderer',
-    emptyOutDir: true,
-    assetsDir: './',
-    rollupOptions: {
-      input: {
-        main: './src/main.ts',
-        renderer: './src/renderer.ts',
-      },
-    },
-  },
+  base: './'
 })
